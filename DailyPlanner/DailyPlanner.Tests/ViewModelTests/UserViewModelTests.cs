3260b0c1be368378ab12c22c25d109556714fcad
Viktor Kryvoruchko <deadprome@gmail.com>
2023-06-22 12:59:07 +0200

    Added nUnit test project

---
+++ DailyPlanner/DailyPlanner.Tests/ViewModelTests/UserViewModelTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyPlanner.Tests.ViewModelTests
+{
+    [TestFixture]
+    public class UserViewModelTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void TestCase()
+        {
+
+        }
+    }
+}
